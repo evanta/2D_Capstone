10ff83f7bd77d5ae1f29979c170b2b2875bbb78f
Evan Taylor <taylorevan16@gmail.com>
2026-04-12 20:43:40 -0400

    Added some initial player movement so that the player sprite jumps on a grid.

--- 2d-capstone-main/Sprite/character.gd
+++ 2d-capstone-main/Sprite/character.gd
@@ -1,47 +1,43 @@
 extends CharacterBody2D
 
-@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
-
+@onready var anim = $AnimationPlayer
+@onready var sprite = $AnimatedSprite2D
 @onready var camera_2d: Camera2D = $Camera2D
-@export var cameraZoom : float = 1.1
+@export var cameraZoom: float = 1.1
+@export var moveSpeed: float = 2.0
 
-const speed = 5
-var virtmovement = false
+const tile_size = Vector2(32, 32)
+const vector_down = Vector2(0, tile_size.y)
+const vector_up = Vector2(0, -tile_size.y)
+const vector_right = Vector2(tile_size.x, 0)
+const vector_left = Vector2(-tile_size.x, 0)
+
+var is_moving := false
 
 func _ready():
-	print("start")	
+	print("start")
+	anim.speed_scale = moveSpeed
 	camera_2d.zoom = Vector2(cameraZoom, cameraZoom)
+	sprite.play("idle")
 
+func _input(event):
+	if is_moving:
+		return
+	if event.is_action_pressed("ui_right"):
+		sprite.flip_h = false
+		_move(vector_right, "MoveRight")
+	elif event.is_action_pressed("ui_left"):
+		sprite.flip_h = true
+		_move(vector_left, "MoveLeft")
+	elif event.is_action_pressed("ui_up"):
+		_move(vector_up, "MoveUp")
+	elif event.is_action_pressed("ui_down"):
+		_move(vector_down, "MoveDown")
+
+func _move(direction: Vector2, anim_name: String):
+	is_moving = true
+	anim.play(anim_name)
+	var tween = create_tween()
+	tween.tween_property(self, "position", position + direction, 1.0 / moveSpeed)
+	tween.finished.connect(func(): is_moving = false)
 	
-func _process(delta):
-	#movement 
-	if Input.is_action_pressed("ui_right"):
-		position.x += delta + speed
-	if Input.is_action_pressed("ui_left"):
-		position.x -= delta + speed
-		
-	if Input.is_action_pressed("ui_up"):
-		position.y -= delta + speed
-		virtmovement = true
-	elif Input.is_action_pressed("ui_down"):
-		position.y += delta + speed
-		virtmovement = true
-	else: 
-		virtmovement = false
-		
-	#animation
-	var dir := 0
-	if Input.is_action_pressed("ui_right"):
-		dir = 1
-	elif Input.is_action_pressed("ui_left"):
-		dir = -1
-	
-	if virtmovement == true:
-		anim.play("jump")
-	elif dir != 0:
-		anim.flip_h = (dir < 0)
-		if anim.animation != "jump":
-			anim.play("jump")
-	else:
-		if anim.animation != "idle":
-			anim.play("idle")
